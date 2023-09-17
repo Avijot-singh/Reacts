c98fe32517f854431859b671561917826d18b8af
Avijot-singh <singhavijot17@gmail.com>
2023-09-17 12:56:39 +1000

    Initialising react and modules

--- Test_copy.mjs
+++ Test_copy.mjs
@@ -1,2 +1,6 @@
-import ran, { b,c,d} from './Test.mjs' // Curly brackets is used when the default is not required
+import ran, { b, c, d } from './Test.mjs' // Curly brackets is used when the default is not required
+
 console.log(ran);
+console.log(b);
+console.log(c);
+console.log(d);
